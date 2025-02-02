56eaf1120e7702dbc79bb335a9c281faf62b9c24
ritikseeker <ritikraturiworks@gmail.com>
2025-02-02 08:28:47 -0500

    Completed till item subtotal in CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,46 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
-  };
+  return cart.reduce((total, item) => 
+    total + (item.cost * item.quantity), 0
+    ).toFixed(2);
+    };
+
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping();
   };
 
-
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   const handleIncrement = (item) => {
-  };
+  dispatch(updateQuantity({ 
+    name: item.name, 
+    quantity: item.quantity + 1 
+  }));
+    };
 
-  const handleDecrement = (item) => {
-   
-  };
+    const handleDecrement = (item) => {
+    if (item.quantity === 1) {
+        dispatch(removeItem(item.name));
+    } else {
+        dispatch(updateQuantity({ 
+        name: item.name, 
+        quantity: item.quantity - 1 
+        }));
+    }
+    };
+
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return(item.cost*item.quantity).toFixed(2);
   };
 
   return (
